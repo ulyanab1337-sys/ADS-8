9f502e977cfee2933c71e45a81313ee1d08686cb
ulyanab1337-sys <ulyanab1337@gmail.com>
2026-05-13 09:37:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
+// Copyright 2021 NNTU-CS
 #include "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* filename);
-void printFreq(BST<std::string>& tree);
+void makeTree(BST<std::string> &tree, const char *filename);
+void printFreq(BST<std::string> &tree);
 
 int main() {
-    BST<std::string> tree;
-    makeTree(tree, "src/war_peace.txt");
-    printFreq(tree);
-    return 0;
+  BST<std::string> dictionary;
+  makeTree(dictionary, "src/war_peace.txt");
+  printFreq(dictionary);
+  return 0;
 }
